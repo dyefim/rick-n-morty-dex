d623912655484828524337b0279bfafca6d3330b
dyefim <yefimchuk.dmytro@gmail.com>
2021-07-30 10:49:58 +0300

    minimal setup

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  name: 'client',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  mode: 'development', // production
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname + '/build'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.json',
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  target: 'web',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './build'),
+    historyApiFallback: true,
+  },
+};
